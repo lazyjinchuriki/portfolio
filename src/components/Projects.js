3745b2fc4505fc17ccb3823cef76ab47f8d536b5
Rahul Khushalani <rahulkhushalani@proton.me>
2023-09-07 15:47:15 +0530

    idk why am i doing this

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -151,7 +151,11 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img
+        className="background-image-right"
+        src={colorSharp2}
+        alt="background"
+      ></img>
     </section>
   );
 };
